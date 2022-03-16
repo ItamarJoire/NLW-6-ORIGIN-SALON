e57b46c55bed91c7189a4cece5fc32d265e6cffb
Itamar Joire <ijoire7@gmail.com>
2022-03-16 15:21:52 -0300

    Aula 02 concluída

---
+++ main.js
@@ -0,0 +1,28 @@
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', () => {
+    nav.classList.toggle('show')
+  })
+}
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', () => {
+    nav.classList.remove('show')
+  })
+}
+
+// MUDAR O HEADER DA PÁGINA QUANDO DER O SCROLL
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
